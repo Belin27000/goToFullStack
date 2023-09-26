18bdd69b1783c79bb78272a50d06da19b25bca72
Belin27000 <y.lecerf@protonmail.com>
2023-09-26 16:40:25 +0200

    add controllers

--- backend/server.js
+++ backend/server.js
@@ -45,3 +45,12 @@ server.on('listening', () => {
 });
 
 server.listen(port);
+
+
+
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+});
