b1d5fea0701022489e30e6a0b148494e7532c47b
Belin27000 <y.lecerf@protonmail.com>
2023-09-26 19:51:00 +0200

    add jwt to the app

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff.js')
+const userRoutes = require('./routes/user.js')
 
 mongoose.connect('mongodb+srv://conseilandtechGoToFullStack:sPOFDGwtUFZp5N39@gotofullstack.ckprrkj.mongodb.net/?retryWrites=true&w=majority',
     {
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use('/api/stuff', stuffRoutes)
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
