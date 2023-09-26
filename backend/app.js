a729a92293c5b1fcb2093a27e5aa640a0c41cd98
Belin27000 <y.lecerf@protonmail.com>
2023-09-26 11:51:10 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const { log } = require('console');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff.js');
+
+mongoose.connect('mongodb+srv://conseilandtechGoToFullStack:sPOFDGwtUFZp5N39@gotofullstack.ckprrkj.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.use('.api/stuff', stuffRoutes)
+
+
+module.exports = app;
